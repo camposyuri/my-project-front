25776154048894ec2187f26f0bb4d1878e787727
camposyuri <yurinapoleao@gmail.com>
2021-08-25 20:53:46 -0300

    Building dashboard

--- src/pages/dashboard/dashboard-page.js
+++ src/pages/dashboard/dashboard-page.js
@@ -1,12 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import clsx from "clsx";
 import useStyles from "./dashboard-page.pstl";
+import {
+  AppBar,
+  IconButton,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
 
 const Page = () => {
   const classes = useStyles();
+  const [open, setOpen] = useState(true);
+
+  const handleDrawerToggle = () => {
+    setOpen((prevState) => !prevState);
+  };
 
   return (
-    <div className={classes.dale}>
-      <h1>Hello Dashboard</h1>
+    <div>
+      <AppBar
+        position="absolute"
+        className={clsx(classes.appBar, open && classes.appBarShift)}
+      >
+        <Toolbar className={classes.toolbar}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerToggle}
+            className={clsx(
+              classes.menuButton,
+              open && classes.menuButtonHidden
+            )}
+          >
+            <MenuItem />
+          </IconButton>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            className={classes.title}
+          >
+            Dashboard
+          </Typography>
+          <IconButton color="inherit">
+            {/* <Badge badgeContent={4} color="secondary">
+                <NotificationsIcon></NotificationsIcon>
+              </Badge> */}
+          </IconButton>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
